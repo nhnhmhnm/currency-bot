78d23f7a29fb0cc081e30d3cfdd2af2fd3a76473
nhnhmhnm <onep5286@gmail.com>
2025-07-21 17:08:02 +0900

    실제 실행 파일

---
+++ crawler/scheduler.py
@@ -0,0 +1,14 @@
+import schedule
+import time
+from main import crawl_all
+
+print("[시작] 환율 크롤러")
+
+crawl_all()  # 최초 실행
+
+# 10분마다 실행
+schedule.every(10).minutes.do(crawl_all)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
