b78f53a6c9d65b79c36f8a55bc3df7d8513d2cba
nhnhmhnm <onep5286@gmail.com>
2025-07-21 17:06:24 +0900

    임시 setting

---
+++ crawler/config.py
@@ -0,0 +1,10 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "root",
+    "database": "exchange_tracker",
+    "charset": "utf8mb4",
+    "use_unicode": True
+    }
+
+CHROME_DRIVER_PATH = "C:\\chromedriver\\chromedriver.exe"
