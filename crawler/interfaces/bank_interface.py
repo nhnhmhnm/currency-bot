14c09c1baa578874e21c3838989a2cb76a3d0e9f
nhnhmhnm <onep5286@gmail.com>
2025-07-23 13:33:12 +0900

    get_datas 리턴 자료형 변경

--- crawler/interfaces/bank_interface.py
+++ crawler/interfaces/bank_interface.py
@@ -1,9 +1,10 @@
 from typing import Protocol
 from utils.enums import Bank
+from dto.exchange_rate_dto import ExchangeRateDTO
 
 class BankCrawler(Protocol):
     def get_bank(self) -> Bank:
         ...
 
-    def get_datas(self) -> list[dict]:
+    def get_datas(self) -> ExchangeRateDTO:
         ...
