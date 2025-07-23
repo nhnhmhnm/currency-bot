c5bb2e2521911dacd769e9aec4b83a46ace70dda
nhnhmhnm <onep5286@gmail.com>
2025-07-23 13:33:53 +0900

    dto로 전달하게 수정

--- crawler/utils/crawler.py
+++ crawler/utils/crawler.py
@@ -1,26 +1,29 @@
 from utils.enums import Bank, Currency
 from utils.DBconnect import insert_rate
 from interfaces.bank_interface import BankCrawler
-from banks.shinhan import get_shinhan
+from banks.shinhan import ShinhanCrawler
 from banks.ibk import get_ibk
-
+from banks.sc import ScCrawler
+from banks.hana import HanaCrawler
 
 crawlers: list[BankCrawler] = [
-    get_shinhan()
+    ShinhanCrawler(),
+    ScCrawler(),
+    HanaCrawler()
 ]
 
 def crawl_all():
     for crawler in crawlers:
         try:
-            results = crawler.get_datas()
+            results = crawler.get_datas() # list[ExchangeRateDTO]
 
             if not results:
                 print(f"[WARN] {crawler.get_bank().value}: 크롤링 실패")
                 continue
 
-            for result in results:
-                insert_rate(**result)
-                print(f"[SUCCESS] {result['bank'].value} {result['currency'].value} 저장 완료")
+            for dto in results:
+                insert_rate(dto)
+                print(f"[SUCCESS] {dto.bank.value} {dto.currency.value} 저장 완료")
 
         except Exception as e:
             print(f"[ERROR] {crawler.get_bank().value} 처리 중 예외 발생: {e}")
