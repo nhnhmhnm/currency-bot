0c2b899f0af7c54e722c40b4cc1b27e4c0ceb18f
nhnhmhnm <onep5286@gmail.com>
2025-08-05 11:09:52 +0900

    interface 생성

--- backend/src/main/kotlin/org/example/backend/exchange/service/LedgerService.kt
+++ backend/src/main/kotlin/org/example/backend/exchange/service/LedgerService.kt
@@ -4,6 +4,5 @@ import org.example.backend.exchange.dto.ExchangeLedgerDTO
 import java.math.BigDecimal
 
 interface LedgerService {
-//    fun record(userId: Long, currencyCode: String, amount: BigDecimal, type: String): ExchangeLedgerDTO
-
+    fun record(ledger: ExchangeLedgerDTO): ExchangeLedgerDTO
 }
