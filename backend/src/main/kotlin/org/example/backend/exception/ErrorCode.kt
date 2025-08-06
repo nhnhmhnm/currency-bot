f3e864cffc5055f48bc3e3b6ba7a8f71a0d6a196
naegwon <joonwoo9192@gmail.com>
2025-08-06 18:21:27 +0900

    에러 코드 수정

--- backend/src/main/kotlin/org/example/backend/exception/ErrorCode.kt
+++ backend/src/main/kotlin/org/example/backend/exception/ErrorCode.kt
@@ -1,9 +1,12 @@
 package org.example.backend.exception
 
+import org.springframework.http.HttpStatus
+
 enum class ErrorCode (
+  val httpStatus: HttpStatus,
   val code: Int,
   val message: String
 ) {
-  DUPLICATED_USER_DEVICE(101, "duplicated user device"),
-  FAILED_TO_CREATE_USER_ID(102, "failed to create user id")
+  DUPLICATED_USER_DEVICE(HttpStatus.BAD_REQUEST, 101, "duplicated user device"),
+  FAILED_TO_CREATE_USER_ID(HttpStatus.INTERNAL_SERVER_ERROR, 102, "failed to create user id")
 }
