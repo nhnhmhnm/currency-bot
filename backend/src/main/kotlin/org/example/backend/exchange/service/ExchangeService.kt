af394779f901ad88e2518f530317c54549a444e0
nhnhmhnm <onep5286@gmail.com>
2025-08-07 15:27:51 +0900

    매매기준율 기준 order 메소드 사기 팔기 분리

--- backend/src/main/kotlin/org/example/backend/exchange/service/ExchangeService.kt
+++ backend/src/main/kotlin/org/example/backend/exchange/service/ExchangeService.kt
@@ -1,13 +1,17 @@
 package org.example.backend.exchange.service
 
 import org.example.backend.exchange.dto.ExchangeDTO
+import org.example.backend.finance.domain.Currency
+import org.example.backend.finance.repository.CurrencyRepository
 import java.math.BigDecimal
 
 interface ExchangeService {
     fun getBestBuyRate(currencyCode: String): ExchangeDTO
-
     fun getBestSellRate(currencyCode: String): ExchangeDTO
 
     // 매매기준율 기준
-    fun getBestArbitrageRate(currencyCode: String): Pair<ExchangeDTO, ExchangeDTO>
+    fun getBestBuyBaseRate(currencyCode: String): ExchangeDTO
+    fun getBestSellBaseRate(currencyCode: String): ExchangeDTO
+
+    fun calculateExchange(fromCurrency: Currency, toCurrency: Currency, exchangeRate: BigDecimal, fromAmount: BigDecimal): Pair<BigDecimal, BigDecimal>
 }
