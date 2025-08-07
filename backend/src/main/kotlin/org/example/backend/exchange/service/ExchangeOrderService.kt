d873ca84053792bd4b036504be06a44ab5372849
nhnhmhnm <onep5286@gmail.com>
2025-08-07 15:26:22 +0900

    파라미터 추가

--- backend/src/main/kotlin/org/example/backend/exchange/service/ExchangeOrderService.kt
+++ backend/src/main/kotlin/org/example/backend/exchange/service/ExchangeOrderService.kt
@@ -4,8 +4,8 @@ import org.example.backend.exchange.dto.ExchangeOrderDTO
 import java.math.BigDecimal
 
 interface ExchangeOrderService {
-    fun buyOrder(userId: Long, currencyCode: String, amount: BigDecimal): ExchangeOrderDTO
-    fun sellOrder(userId: Long, currencyCode: String, amount: BigDecimal): ExchangeOrderDTO
+    fun buyOrder(userId: Long, currencyCode: String, amount: BigDecimal, isArbitrage: Boolean): ExchangeOrderDTO
+    fun sellOrder(userId: Long, currencyCode: String, amount: BigDecimal, isArbitrage: Boolean): ExchangeOrderDTO
 
     fun arbitrageOrder(userId: Long, currencyCode: String, amount: BigDecimal): Pair<ExchangeOrderDTO, ExchangeOrderDTO>
 }
