307c51fd0972a879f16fb4917016f47fd4bea64d
nhnhmhnm <onep5286@gmail.com>
2025-08-12 14:30:20 +0900

    주석 수정

--- backend/src/main/kotlin/org/example/backend/user/service/WalletService.kt
+++ backend/src/main/kotlin/org/example/backend/user/service/WalletService.kt
@@ -9,50 +9,16 @@ interface WalletService {
     fun depositFromAccount(userId: Long, currencyId: Long, amount: BigDecimal): BigDecimal // 연결된 계좌에서 SUPER 계좌로 입금 -> 확인되면 지갑에 입금
     fun withdrawToAccount(userId: Long, currencyId: Long, amount: BigDecimal): BigDecimal // 지갑에서 연결된 계좌로 출금
 
-
-    /**
-     회사 계좌 -> 유저 지갑 depositFromAccount
-
-     buy
-    최종 환전된 금액 toAmount
-
-    sell
-    유저 최종 환전 금액 toAmount
-     **/
+    // 회사 계좌 -> 유저 지갑
     fun companyToUser(accountId: Long, userId: Long, currencyId: Long, amount: BigDecimal): BigDecimal
 
-    /**
-     유저 지갑 -> 회사 계좌 withdrawToAccount
-
-    buy
-    수수료 commissionAmount
-    실제 환전할 원화 exchangeAmount
-
-    sell
-    환전할 달러 toAmount
-
-     **/
+    // 유저 지갑 -> 회사 계좌
     fun userToCompany(userId: Long, currencyId: Long, accountId: Long, amount: BigDecimal): BigDecimal
 
     // 회사 계좌 -> 회사 계좌
-    // sell 후 환전된 금액에서 수수료, 차익을 계산하여 회사 계좌로 입금
+    // sell 후 환전된 금액에서 수수료를 계산하여 회사 수수료 계좌로 입금
     fun companyToCompany(fromAccountId: Long, toAccountId: Long, amount: BigDecimal): BigDecimal
 
-    /*
-    은행 -> 회사 계좌
-    // 회사 계좌 잔액 증가/ 감소
-    // userid, accountid, amount
-
-    buy
-     환전된 금액 toAmount 증가/감소
-
-     sell
-     환전할 달러 toAmount 증가/감소
-    환전된 금액 rawToAmount
-     */
-
-    // 은행에서 환전한 금액을 회사 계좌로 입금
+    // 은행-회사 사이에서 환전한 금액을 회사 계좌로 입금
     fun bankToCompany(accountId: Long, bankId: Long, amount: BigDecimal): BigDecimal
-
-
 }
